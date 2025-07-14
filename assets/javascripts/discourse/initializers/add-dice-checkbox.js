0eca43244c85a21144f45140ea621f228bf3c585
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-14 16:52:21 +0900

    widget 수정

--- assets/javascripts/discourse/initializers/add-dice-checkbox.js
+++ assets/javascripts/discourse/initializers/add-dice-checkbox.js
@@ -4,49 +4,53 @@ export default {
   name: "dice-comment-checkbox",
   initialize() {
     withPluginApi("0.8.7", (api) => {
-
-      // 1. 저장 시 값 전달
-      api.addComposerSaveOptionsCallback((model, saveOptions) => {
-        if (model.creatingTopic) {
-          saveOptions.dice_only = model.dice_only;
-          saveOptions.dice_max = model.dice_max;
-        }
-      });
-
-      // 2. composer model 확장 및 DOM 삽입
+      // 1. 저장 시 custom_fields로 값 전달
       api.modifyClass("controller:composer", {
         pluginId: "discourse-dice-comment",
-        didInsertElement() {
-          this._super(...arguments);
 
-          if (!this.model.creatingTopic) return;
-          if (this.model.dice_only === undefined) this.model.dice_only = false;
-          if (this.model.dice_max === undefined) this.model.dice_max = 100;
-
-          setTimeout(() => {
-            const composerFields = document.querySelector(".composer-fields");
-            if (!composerFields || document.querySelector(".dice-only-wrapper")) return;
-
-            const wrapper = document.createElement("div");
-            wrapper.className = "dice-only-wrapper";
-            wrapper.innerHTML = `
-              <label>
-                <input type="checkbox" class="dice-only-checkbox" />
-                주사위댓글 전용
-              </label>
-              <input type="number" class="dice-max" value="${this.model.dice_max}" min="0" style="margin-left: 1em;">
-            `;
-            composerFields.appendChild(wrapper);
-
-            wrapper.querySelector(".dice-only-checkbox").addEventListener("change", (e) => {
-              this.model.dice_only = e.target.checked;
-            });
-
-            wrapper.querySelector(".dice-max").addEventListener("change", (e) => {
-              this.model.dice_max = parseInt(e.target.value, 10);
-            });
-          }, 100);
+        actions: {
+          publish() {
+            if (this.model.action === "createTopic") {
+              this.model.set("dice_only", this.model.dice_only ?? false);
+              this.model.set("dice_max", this.model.dice_max ?? 100);
+            }
+            return this._super(...arguments);
+          },
         },
+
+        init() {
+          this._super(...arguments);
+          if (this.model.creatingTopic) {
+            if (this.model.dice_only === undefined) this.model.dice_only = false;
+            if (this.model.dice_max === undefined) this.model.dice_max = 100;
+          }
+        },
+      });
+
+      // 2. composer UI에 요소 삽입 (위젯 기반)
+      api.decorateWidget("composer-fields:after", (helper) => {
+        const model = helper.widget.model;
+        if (model.action !== "createTopic") return;
+
+        const checkbox = helper.h("label.dice-only", [
+          helper.h("input.dice-only-checkbox", {
+            type: "checkbox",
+            checked: model.dice_only,
+            onchange: (e) => (model.dice_only = e.target.checked),
+          }),
+          helper.h("span", helper.widget._t("dice_comment.checkbox")),
+        ]);
+
+        const maxInput = model.dice_only
+          ? helper.h("input.dice-max", {
+              type: "number",
+              value: model.dice_max,
+              min: 0,
+              onchange: (e) => (model.dice_max = parseInt(e.target.value, 10)),
+            })
+          : null;
+
+        return helper.h("div.dice-only-wrapper", [checkbox, maxInput]);
       });
     });
   },
